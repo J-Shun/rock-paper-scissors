6d938a987014927363e9f5391395df5bfbeb1914
Shun <ter0123540@gmail.com>
2022-04-07 22:08:59 +0800

    create PlayerSelection function

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ function computerPlay() {
     return choices[random];
 }
 
+function playerSelection() {
+    let playerChoice = prompt(`Please enter "ROCK", "PAPER", or "SCISSORS"`);
+    while (playerChoice != "ROCK" && playerChoice != "PAPER" && playerChoice != "SCISSORS") {
+        playerChoice = prompt(`Invalid Enter! Please enter "ROCK", "PAPER", or "SCISSORS"`);
+    }
+    return playerChoice;
+}
 
-
+console.log(playerSelection());
