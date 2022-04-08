9aedcce20b7701e0585f67a6fd38a8b5c35a1cab
Shun <ter0123540@gmail.com>
2022-04-08 23:14:33 +0800

    create game function to set the game

--- app.js
+++ app.js
@@ -16,14 +16,41 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") 
     || (playerSelection === "PAPER" && computerSelection === "ROCK") 
     || (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        return 1;
     } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") 
     || (playerSelection === "PAPER" && computerSelection === "SCISSORS") 
     || (playerSelection === "SCISSORS" && computerSelection === "ROCK")) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return -1;
     } else {
-        return `It's a draw!`;
+        console.log(`It's a draw!`);
+        return 0;
     }
 }
 
-console.log(playRound(playerPlay(), computerPlay()));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let result = 0;
+
+    for (let i = 0; i < 5; i++) {
+        result = playRound(playerPlay(), computerPlay());
+        if (result === 1) {
+            playerScore++;
+        } else if (result === -1) {
+            computerScore++;
+        }
+        console.log(`Now you got ${playerScore}, and computer got ${computerScore}`);
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`Game set! You win the game!`);
+    } else if (playerScore < computerScore) {
+        console.log(`Game set! You lose the game`);
+    } else {
+        console.log(`Game set! No one wins`);
+    }
+}
+
+game();
